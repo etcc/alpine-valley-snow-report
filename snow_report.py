b84e0c59cf45384ac1d98f523313fecc55714046
Ben Keating <ben@deepdream.com>
2016-01-26 22:23:58 -0600

    Updated URL and xpaths to work with Alpine's new site design

--- snow_report.py
+++ snow_report.py
@@ -5,7 +5,7 @@ Copyright 02014 Ben Keating (http://bpk.deepdream.com)
 
 Originally written for the East Troy Area Chamber of Commerce so we could 
 display Alpine Valley's current snow report directly on easttroy.org. This 
-module greatly depends on the structure of alpinevalleyresort.com/snow-report.php. 
+module greatly depends on the structure of alpinevalleyresort.com/. 
 Please don't change it, Alpine! Just keep it updated. <3
 """
 
@@ -17,55 +17,57 @@ DEBUG = True
 
 def get_snow_report():
 
-    page = requests.get('http://www.alpinevalleyresort.com/snow-report.php')
+    page = requests.get('http://www.alpinevalleyresort.com/the-mountain/snow-report/')
     tree = html.fromstring(page.text)
 
-    status = tree.xpath('//*[@id="detail-weather"]/tr[6]/th/table/tr[1]/td[2]/span/text()')[0]
-    status_full = tree.xpath('//*[@id="body-copy"]/table[2]/tr[1]/td/text()')[0]
-    hours = tree.xpath('//*[@id="detail-weather"]/tr[6]/th/table/tr[2]/td[2]/text()')[0]
-    trail_status = tree.xpath('//*[@id="detail-weather"]/tr[6]/th/table/tr[3]/td[2]/text()')[0]
-    lift_status = tree.xpath('//*[@id="detail-weather"]/tr[6]/th/table/tr[4]/td[2]/text()')[0]
-    snow_base = tree.xpath('//*[@id="detail-weather"]/tr[6]/th/table/tr[5]/td[2]/text()')[0]
-    snow_yesterday = tree.xpath('//*[@id="detail-weather"]/tr[6]/th/table/tr[6]/td[2]/text()')[0]
-    currently = tree.xpath('//*[@id="detail-weather"]/tr[6]/th/table/tr[7]/td[2]/text()')[0]
-    condition_description = tree.xpath('//*[@id="body-copy"]/p[1]/text()')[0]
+    status = tree.xpath('/html/body/div[1]/div[2]/div[1]/div[1]/div[2]/div[1]/text()')[0]
+    hours = tree.xpath('/html/body/div[1]/div[2]/div[1]/div[1]/div[2]/div[2]/text()')[0]
+    trail_status = tree.xpath('/html/body/div[1]/div[2]/div[1]/div[1]/div[2]/div[3]/div/text()')[0]
+    lift_status = tree.xpath('/html/body/div[1]/div[2]/div[1]/div[1]/div[2]/div[4]/div/text()')[0]
+    snow_base = tree.xpath('/html/body/div[1]/div[2]/div[1]/div[1]/div[2]/div[5]/div/text()')[0]
+    snow_yesterday = tree.xpath('/html/body/div[1]/div[2]/div[2]/div[3]/p[2]/text()[3]')[0]
+    currently = tree.xpath('/html/body/div[1]/div[2]/div[1]/div[1]/div[2]/div[6]/div/text()')[0]
+    condition_description = tree.xpath('/html/body/div[1]/div[2]/div[2]/div[3]/p[3]/text()')[0]
+    last_updated = tree.xpath('/html/body/div[1]/div[2]/div[2]/div[3]/p[1]/text()')[0]
     polled_at = datetime.datetime.now()
 
     trail_list = {
-        'E-Z-Does-it': tree.xpath('//*[@id="body-copy"]/table[2]/tr[2]/td/table[1]/tr[2]/td[2]/text()')[0],
-        'First Adventure': tree.xpath('//*[@id="body-copy"]/table[2]/tr[2]/td/table[1]/tr[3]/td[2]/text()')[0],
-        'Raspberry': tree.xpath('//*[@id="body-copy"]/table[2]/tr[2]/td/table[1]/tr[4]/td[2]/text()')[0],
-        'Soft Landings': tree.xpath('//*[@id="body-copy"]/table[2]/tr[2]/td/table[1]/tr[5]/td[2]/text()')[0],
-        'Strawberry': tree.xpath('//*[@id="body-copy"]/table[2]/tr[2]/td/table[1]/tr[6]/td[2]/text()')[0],
-        'Valley View': tree.xpath('//*[@id="body-copy"]/table[2]/tr[2]/td/table[1]/tr[7]/td[2]/text()')[0],
-        'Cut Throat': tree.xpath('//*[@id="body-copy"]/table[2]/tr[2]/td/table[1]/tr[8]/td[2]/text()')[0],
-        'E-Z Pass': tree.xpath('//*[@id="body-copy"]/table[2]/tr[2]/td/table[1]/tr[9]/td[2]/text()')[0],
-        'East Pass': tree.xpath('//*[@id="body-copy"]/table[2]/tr[2]/td/table[1]/tr[10]/td[2]/text()')[0],
-        'Halfway': tree.xpath('//*[@id="body-copy"]/table[2]/tr[2]/td/table[1]/tr[11]/td[2]/text()')[0],
-        'Northwest Pass': tree.xpath('//*[@id="body-copy"]/table[2]/tr[2]/td/table[1]/tr[12]/td[2]/text()')[0],
-        'Timber Gulch': tree.xpath('//*[@id="body-copy"]/table[2]/tr[2]/td/table[1]/tr[13]/td[2]/text()')[0],
-        'Timber Trail': tree.xpath('//*[@id="body-copy"]/table[2]/tr[2]/td/table[1]/tr[14]/td[2]/text()')[0],
-        'Alpine': tree.xpath('//*[@id="body-copy"]/table[2]/tr[2]/td/table[1]/tr[15]/td[2]/text()')[0],
-        'Big Thunder': tree.xpath('//*[@id="body-copy"]/table[2]/tr[2]/td/table[1]/tr[16]/td[2]/text()')[0],
-        'Broadway': tree.xpath('//*[@id="body-copy"]/table[2]/tr[2]/td/table[1]/tr[17]/td[2]/text()')[0],
-        'Lodge': tree.xpath('//*[@id="body-copy"]/table[2]/tr[2]/td/table[1]/tr[18]/td[2]/text()')[0],
-        'Mohawk': tree.xpath('//*[@id="body-copy"]/table[2]/tr[2]/td/table[1]/tr[19]/td[2]/text()')[0],
-        'Shelter': tree.xpath('//*[@id="body-copy"]/table[2]/tr[2]/td/table[1]/tr[20]/td[2]/text()')[0]
+        '1st Tracks': tree.xpath('/html/body/div[1]/div[2]/div[2]/div[3]/table[1]/tbody/tr[1]/td[2]/text()')[0],
+        'Baby Steps': tree.xpath('/html/body/div[1]/div[2]/div[2]/div[3]/table[1]/tbody/tr[2]/td[2]/text()')[0],
+        'E-Z-Does-it': tree.xpath('/html/body/div[1]/div[2]/div[2]/div[3]/table[1]/tbody/tr[3]/td[2]/text()')[0],
+        'First Adventure': tree.xpath('/html/body/div[1]/div[2]/div[2]/div[3]/table[1]/tbody/tr[4]/td[2]/text()')[0],
+        'Snowbank': tree.xpath('/html/body/div[1]/div[2]/div[2]/div[3]/table[1]/tbody/tr[5]/td[2]/text()')[0],
+        'Soft Landings': tree.xpath('/html/body/div[1]/div[2]/div[2]/div[3]/table[1]/tbody/tr[6]/td[2]/text()')[0],
+        'Valley View': tree.xpath('/html/body/div[1]/div[2]/div[2]/div[3]/table[1]/tbody/tr[7]/td[2]/text()')[0],
+        'Cut Throat': tree.xpath('/html/body/div[1]/div[2]/div[2]/div[3]/table[1]/tbody/tr[8]/td[2]/text()')[0],
+        'E-Z Pass': tree.xpath('/html/body/div[1]/div[2]/div[2]/div[3]/table[1]/tbody/tr[9]/td[2]/text()')[0],
+        'East Pass': tree.xpath('/html/body/div[1]/div[2]/div[2]/div[3]/table[1]/tbody/tr[10]/td[2]/text()')[0],
+        'Halfway': tree.xpath('/html/body/div[1]/div[2]/div[2]/div[3]/table[1]/tbody/tr[11]/td[2]/text()')[0],
+        'Northwest Pass': tree.xpath('/html/body/div[1]/div[2]/div[2]/div[3]/table[1]/tbody/tr[12]/td[2]/text()')[0],
+        'Timber Gulch': tree.xpath('/html/body/div[1]/div[2]/div[2]/div[3]/table[1]/tbody/tr[13]/td[2]/text()')[0],
+        'Timber Trail': tree.xpath('/html/body/div[1]/div[2]/div[2]/div[3]/table[1]/tbody/tr[14]/td[2]/text()')[0],
+        'Alpine': tree.xpath('/html/body/div[1]/div[2]/div[2]/div[3]/table[1]/tbody/tr[15]/td[2]/text()')[0],
+        'Big Thunder': tree.xpath('/html/body/div[1]/div[2]/div[2]/div[3]/table[1]/tbody/tr[16]/td[2]/text()')[0],
+        'Broadway': tree.xpath('/html/body/div[1]/div[2]/div[2]/div[3]/table[1]/tbody/tr[17]/td[2]/text()')[0],
+        'Lodge': tree.xpath('/html/body/div[1]/div[2]/div[2]/div[3]/table[1]/tbody/tr[18]/td[2]/text()')[0],
+        'Mohawk': tree.xpath('/html/body/div[1]/div[2]/div[2]/div[3]/table[1]/tbody/tr[19]/td[2]/text()')[0],
+        'Shelter': tree.xpath('/html/body/div[1]/div[2]/div[2]/div[3]/table[1]/tbody/tr[20]/td[2]/text()')[0],
     }
 
     lift_list = {
-        'Adding chairlifts as needed': tree.xpath('//*[@id="body-copy"]/table[2]/tr[2]/td/table[2]/tr[2]/td[2]/text()')[0],
-        'Conveyer #1': tree.xpath('//*[@id="body-copy"]/table[2]/tr[2]/td/table[2]/tr[3]/td[2]/text()')[0],
-        'Conveyer #2': tree.xpath('//*[@id="body-copy"]/table[2]/tr[2]/td/table[2]/tr[4]/td[2]/text()')[0],
-        'Conveyer #3': tree.xpath('//*[@id="body-copy"]/table[2]/tr[2]/td/table[2]/tr[5]/td[2]/text()')[0],
-        'Conveyer #4': tree.xpath('//*[@id="body-copy"]/table[2]/tr[2]/td/table[2]/tr[6]/td[2]/text()')[0],
-        'Double - Valley View Chair': tree.xpath('//*[@id="body-copy"]/table[2]/tr[2]/td/table[2]/tr[6]/td[2]/text()')[0],
-        'Quad - E-Z Rider': tree.xpath('//*[@id="body-copy"]/table[2]/tr[2]/td/table[2]/tr[8]/td[2]/text()')[0],
-        'Quad - Valley Flyer': tree.xpath('//*[@id="body-copy"]/table[2]/tr[2]/td/table[2]/tr[9]/td[2]/text()')[0],
-        'Quad- Super Glide': tree.xpath('//*[@id="body-copy"]/table[2]/tr[2]/td/table[2]/tr[10]/td[2]/text()')[0],
-        'Triple - First Adventure Chair': tree.xpath('//*[@id="body-copy"]/table[2]/tr[2]/td/table[2]/tr[11]/td[2]/text()')[0],
-        'Triple - Lodge Chair': tree.xpath('//*[@id="body-copy"]/table[2]/tr[2]/td/table[2]/tr[12]/td[2]/text()')[0],
-        'Triple - Mohawk Chair': tree.xpath('//*[@id="body-copy"]/table[2]/tr[2]/td/table[2]/tr[13]/td[2]/text()')[0],
+        'Adding chairlifts as needed': tree.xpath('/html/body/div[1]/div[2]/div[2]/div[3]/table[2]/tbody/tr[1]/td[2]/text()')[0],
+        'Conveyer #1': tree.xpath('/html/body/div[1]/div[2]/div[2]/div[3]/table[2]/tbody/tr[2]/td[2]/text()')[0],
+        'Conveyer #2': tree.xpath('/html/body/div[1]/div[2]/div[2]/div[3]/table[2]/tbody/tr[3]/td[2]/text()')[0],
+        'Conveyer #3': tree.xpath('/html/body/div[1]/div[2]/div[2]/div[3]/table[2]/tbody/tr[4]/td[2]/text()')[0],
+        'Conveyer #4': tree.xpath('/html/body/div[1]/div[2]/div[2]/div[3]/table[2]/tbody/tr[5]/td[2]/text()')[0],
+        'Conveyer #5': tree.xpath('/html/body/div[1]/div[2]/div[2]/div[3]/table[2]/tbody/tr[6]/td[2]/text()')[0],
+        'Double - Valley View Chair': tree.xpath('/html/body/div[1]/div[2]/div[2]/div[3]/table[2]/tbody/tr[7]/td[2]/text()')[0],
+        'Quad - E-Z Rider': tree.xpath('/html/body/div[1]/div[2]/div[2]/div[3]/table[2]/tbody/tr[8]/td[2]/text()')[0],
+        'Quad - Valley Flyer': tree.xpath('/html/body/div[1]/div[2]/div[2]/div[3]/table[2]/tbody/tr[9]/td[2]/text()')[0],
+        'Quad- Super Glide': tree.xpath('/html/body/div[1]/div[2]/div[2]/div[3]/table[2]/tbody/tr[10]/td[2]/text()')[0],
+        'Triple - First Adventure Chair': tree.xpath('/html/body/div[1]/div[2]/div[2]/div[3]/table[2]/tbody/tr[11]/td[2]/text()')[0],
+        'Triple - Lodge Chair': tree.xpath('/html/body/div[1]/div[2]/div[2]/div[3]/table[2]/tbody/tr[12]/td[2]/text()')[0],
+        'Triple - Mohawk Chair': tree.xpath('/html/body/div[1]/div[2]/div[2]/div[3]/table[2]/tbody/tr[13]/td[2]/text()')[0],
     }
 
     if DEBUG == True:
@@ -75,10 +77,9 @@ def get_snow_report():
             print "\n###\n# Alpine Valley Ski Report Snow Report\n###" 
             print "Data retrieved at %s" % polled_at           
             print "Status: %s" % status
-            print "Status full: %s" % status_full
             print "Hours: %s" % hours
-            print "Trail Status: %s" % trail_status
-            print "Lift Status: %s" % lift_status
+            print "Trail Status: %s of 20" % trail_status
+            print "Lift Status: %s of 12" % lift_status
             print "Snow base: %s" % snow_base
             print "Snow yesterday: %s" % snow_yesterday
             print "Currently: %s" % currently
